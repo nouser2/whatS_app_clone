9e4797100f380179b81304bfeaffb6395b414888
nouser3 <heyiamhere05@gmail.com>
2023-03-03 02:39:19 +0530

    debugging

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\helloknc\Desktop\flkutter\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\helloknc\Desktop\flkutter\whats app clone\whatuoop_cl"
+export "FLUTTER_APPLICATION_PATH=C:\Users\helloknc\Desktop\flkutter\done\whatuoop_cl"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\helloknc\Desktop\flkutter\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\helloknc\Desktop\flkutter\whats app clone\whatuoop_cl"
+export "FLUTTER_APPLICATION_PATH=C:\Users\helloknc\Desktop\flkutter\done\whatuoop_cl"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
